e3f911f3b1f00015578777c3c115047a61ae9875
Yash Ahirrao <yashahirrao204@gmail.com>
2025-03-06 11:36:05 +0530

    all update

--- M1Q4/src/components/MovieList.jsx
+++ M1Q4/src/components/MovieList.jsx
@@ -1,18 +1,21 @@
-import useMovies from "../hooks/useMovies";
-import MovieCard from "./MovieCard";
-
-const MovieList = () => {
-    const { movies } = useMovies();
-
+const MovieList = ({ movies, onSelect }) => {
     return (
-        <div className="movie-list">
-            {movies.length > 0 ? (
-                movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
-            ) : (
-                <p>No movies found.</p>
-            )}
-        </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
+        {movies.map((movie) => (
+          <div key={movie.id} className="border p-4 rounded shadow-lg">
+            <img src={movie.image?.medium || "https://via.placeholder.com/150"} alt={movie.name} className="w-full h-40 object-cover rounded" />
+            <h3 className="mt-2 text-lg font-bold">{movie.name}</h3>
+            <button
+              className="bg-blue-600 text-white px-4 py-2 mt-2 rounded"
+              onClick={() => onSelect(movie)}
+            >
+              Select
+            </button>
+          </div>
+        ))}
+      </div>
     );
-};
-
-export default MovieList;
+  };
+  
+  export default MovieList;
+  
